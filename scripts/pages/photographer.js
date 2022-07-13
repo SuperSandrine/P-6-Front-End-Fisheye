153272dc0c9cfd1ab4de92c088e31ee22e0f0ba6
SuperSandrine <sandrine.mestas@gmail.com>
2022-07-13 14:41:46 +0200

    mise en place de l'appel au paramètre

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,8 +1,14 @@
 //Mettre le code JavaScript lié à la page photographer.html// +++++++++++++++++++++ça marche, mais est-ce essentiel de créer une variable?
 
-// my lets declared and to complete later.
+// my declared lets
+// datas stored
 let photographers = [];
 let media = [];
+let photographerData;
+// current url
+let activeUrl = new URL(window.location.href);
+let parameterID = activeUrl.searchParams.get("id");
+console.log(parameterID);
 
 // Cette fonction va chercher la données,
 // puis la transforme en JSON
@@ -15,7 +21,6 @@ async function getPhotographers() {
     .then((response2) => {
       photographers = response2.photographers; //>>> si la let photographers != de constance)
     });
-  console.log(photographers);
   return { photographers };
 }
 
@@ -24,8 +29,7 @@ async function getMedia() {
     .then((response) => response.json())
     .then((response2) => {
       media = response2.media; //>>> si la let photographers != de constance)
-    })
-    .then(console.log(media));
+    });
   return { media };
 }
 // todo: comment savoir si ça marche? = je joue la fonction:
@@ -34,35 +38,36 @@ async function getMedia() {
 // c'est bien en tableau plein
 getMedia();
 
-// this function Display the data catched previously, it's a "presentation" function
-async function displayData(photographers) {
-  // class name of the section in main, currently the only section of main.
-  const photographersSection = document.querySelector(".photographer_section");
-  photographers.forEach((photographer) => {
-    // in the big function in factories files, for each photographer, we
-    // call the photographerFactory and put it in const photographModel
-    const photographerModel = photographerFactory(photographer);
-    // for each photographMOdel, we call the getUserCardDOM function and put it in the
-    // const userCardDOM
-    const userCardDOM = photographerModel.getUserCardDOM();
-    // In the main/PhotorapherSection, add a node at the end of the list of a const
-    //userCardDOM.
-    photographersSection.appendChild(userCardDOM);
-  });
-}
-// Where is await? Await should be where we'll call the function, at
-// this moment we have a function but, we havent call it yet.
+// avec l'id, récupérer les autres données dans le tableau:
+// j'arrive à récupérer la partie de l'array dans la console avec la deuxième partie
+// de cet ordre.
+// TODO: en gros faut que je dise d'attendre d'avoir le tableau photographers : comment?
+photographerData = photographers.filter((el) => el.id == parameterID);
 
-// async Function to init:
-async function init() {
-  // In the function init, const of object Photographers wait that
-  // the function getPhotographers doing its job and return the
-  // promise (result or error).
-  const { photographers } = await getPhotographers();
-  // Todo: que veut dire les accolades ci-dessus?
-  // once the promise gotten, it apply the function displayData
-  displayData(photographers);
-  // TODO: qu'est ce display data, écrire la fonction ou est-ce une méthode?
+//Plan:
+//  OK=> récupérer l'adresse URl courante: https://www.journaldunet.fr/web-tech/developpement/1202481-comment-recuperer-le-chemin-de-l-url-courante-dans-jquery-et-l-assigner-a-une-variable/#:~:text=Pour%20cela%2C%20JavaScript%20poss%C3%A8de%20nativement,valeur%20de%20l'attribut%20pathname.
+//  OK =>récupérer l'id du paramètre pour afficher ses données dans le display : https://waytolearnx.com/2019/10/comment-recuperer-les-parametres-durl-en-javascript.html
+//  avec l'id, récupérer les autres données dans le tableau avec .find, .filter,
+//.indexOf, .includes: https://www.digitalocean.com/community/tutorials/js-array-search-methods-fr
+
+// Plan suite:
+// on a récupéré les infos
+// on affiche les infos
+
+async function displayData(photographer) {
+  console.log(photographer);
+  const photographersSection =
+    document.getElementsByClassName("photograph-header");
+  // photographers.forEach((photographer) => {
+  //   const photographerModel = photographerFactory(photographer);
+  //   const userCardDOM = photographerModel.getUserCardDOM();
+  //   photographersSection.appendChild(userCardDOM);
+  // });
 }
-// call of the async function just previouslty discribed.
+
+async function init() {
+  const { photographers } = await getPhotographers();
+  photographerData = photographers.filter((el) => el.id == parameterID);
+  displayData(photographerData);
+}
 init();
