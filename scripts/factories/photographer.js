5b097f22ea88863e3a40cf1ff0757af4093ddcdd
SuperSandrine <sandrine.mestas@gmail.com>
2022-07-19 14:13:00 +0200

    mis en place de la fonction getPhotographHeader pour injecter le bandeau photographe-header dynamiquement des pages des photographes

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,55 +1,8 @@
-// function photographerFactory(data) {
-//   const { name, city, country, tagline, price, portrait } = data;
-
-//   // todo: avec les 2 clefs des objets json et on obtient data, data
-//   // qui est le paramètre de photographerfactory
-//   // toUn: data est la valeur de const, du coup comment ça marche
-
-//   const picture = `assets/photographers/${portrait}`;
-//   // cette constante va cherhcher l'image dans le fichier sus-cité
-//   // TODO: appeler le portrait dans la const précédente, portrait = nom de l'image
-//   // portrait est un paramètre
-
-//   // todo: comment ces élément sont-ils remplis?
-//   // le getUserCardDOM contient les infos basiques de la page d'accueil
-//   // elle crée les cards ou article de chaque photographe
-//   function getUserCardDOM() {
-//     const article = document.createElement("article");
-//     const img = document.createElement("img");
-//     const city = document.createElement("city");
-//     const country = document.createElement("country");
-//     const tagline = document.createElement("tagline");
-//     const price = document.createElement("price");
-//     img.setAttribute("src", picture); // ici on ajoute l'attribut source avec la
-//     // const picture définit juste avant qui va chercher le fivier dans le bon dossier
-//     //TODO: ajouter l'alt avec le nom dynamiquement
-//     const h2 = document.createElement("h2");
-//     const p = document.createElement("p");
-//     h2.textContent = name;
-//     p.innerHTML = photographer.map(
-//       (photographer) => `
-//     <h4> ${photographer.city}</h4>
-//     `
-//     );
-//     article.appendChild(img);
-//     article.appendChild(h2);
-//     article.appendChild(p);
-//     return article;
-//   }
-//   return { name, picture, getUserCardDOM };
-// }
-// la fonction getUsercardDOMM renvoie un objet avec name, picture et la dite fonction???
-// toUn, comment marche la récursivité?
-
 function photographerFactory(data) {
   const { name, city, id, country, tagline, price, portrait } = data;
 
   const picture = `assets/photographers/${portrait}`;
-  //const linkUrl = `${id-}photographer.html`;
   const linkUrl = `photographer.html?id=${id}`;
-  //TODO: personnaliser la page html avec id? = en rajoutant des paramètres
-  // Todo: comment rajouter des paramètres ? = avec ?
-  // Todo: est ce que j'ai bien compris les paramètres?
 
   function getUserCardDOM() {
     const link = document.createElement("a");
@@ -65,12 +18,100 @@ function photographerFactory(data) {
     <p class="tagline">${tagline}</p>
     <p class="price">${price}€/jour</p></div>`;
     article.appendChild(link);
-
     link.appendChild(img);
     link.appendChild(h2);
     article.appendChild(text);
-
     return article;
   }
-  return { name, picture, getUserCardDOM };
+  // function getPhotographHeader() {
+  //   const photographHeaderInformation = document.createElement("div");
+  //   photographHeaderInformation.innerHTML = `<div class="photograph-header_information">
+  //   <h2>${name}</h2>
+  //   <p class="city">${city + ", " + country}</p>
+  //   <p class="tagline">${tagline}</p>
+  // </div>`;
+  //   photographHeaderInformation.appendChild;
+  //   return photographHeaderInformation;
+  // }
+  // function getPhotographHeader() {
+  //   const photographHeaderInformation = document.createElement("div");
+  //   console.log(photographHeaderInformation);
+  //   const contactButton = document.createElement("button");
+  //   const img = document.createElement("img");
+  //   img.setAttribute("src", picture);
+  //   photographHeaderInformation.innerHTML = `<div class="photograph-header_information">
+  //   <h2>${name}</h2>
+  //   <p class="city">${city + ", " + country}</p>
+  //   <p class="tagline">${tagline}</p>
+  // </div>`;
+  //   photographHeaderInformation.appendChild(contactButton);
+  //   photographHeaderInformation.appendChild(img);
+  //   return photographHeaderInformation;
+  // }
+
+  // function getPhotographHeader() {
+  //   const photographHeader = document.getElementsByClassName(
+  //     "photographer-header"
+  //   );
+
+  //   const photographHeaderInformation = document.createElement("div");
+  //   photographHeaderInformation.setAttribute(
+  //     "class",
+  //     "photograph-header_information"
+  //   );
+  //   console.log(photographHeaderInformation);
+  //   const contactButton = document.createElement("button");
+  //   const img = document.createElement("img");
+  //   img.setAttribute("src", picture);
+  //   photographHeaderInformation.innerHTML = `
+  //   <h2>${name}</h2>
+  //   <p class="city">${city + ", " + country}</p>
+  //   <p class="tagline">${tagline}</p>`;
+  //   photographHeader.appendChild(photographHeaderInformation);
+  //   photographHeader.appendChild(contactButton);
+  //   photographHeader.appendChild(img);
+  //   return photographHeader;
+  // }
+
+  // function getPhotographHeader() {
+  //   const photographHeader = document.createElement("div");
+  //   const photographHeaderInformation = document.createElement("div");
+  //   photographHeaderInformation.setAttribute(
+  //     "class",
+  //     "photograph-header_information"
+  //   );
+  //   const contactButton = document.createElement("button");
+
+  //   const img = document.createElement("img");
+  //   img.setAttribute("src", picture);
+  //   photographHeaderInformation.innerHTML = `
+  //   <h2>${name}</h2>
+  //   <p class="city">${city + ", " + country}</p>
+  //   <p class="tagline">${tagline}</p>`;
+  //   photographHeader.appendChild(photographHeaderInformation);
+  //   photographHeader.appendChild(contactButton);
+  //   photographHeader.appendChild(img);
+  //   console.log(photographHeader);
+
+  //   return photographHeader;
+  // }
+
+  function getPhotographHeader() {
+    const photographHeader = document.createElement("div");
+    photographHeader.innerHTML = `<div class="photograph-header_information">
+      <h2>${name}</h2>
+      <p class="city">${city + ", " + country}</p>
+      <p class="tagline">${tagline}</p>
+    </div>
+    <button class="contact_button" onclick="displayModal()">
+          Contactez-moi
+        </button>
+        <img src="${picture}"/>
+    `;
+    return photographHeader;
+  }
+  // TODO: il y a la création d'une div autour des 3 balises avec la première
+  //ligne, peut-on l'enlever?
+
+  return { name, picture, getUserCardDOM, getPhotographHeader };
 }
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -10,60 +10,31 @@ let activeUrl = new URL(window.location.href);
 let parameterID = activeUrl.searchParams.get("id");
 console.log(parameterID);
 
-// Cette fonction va chercher la données,
+// Cette fonction va chercher les données,
 // puis la transforme en JSON
-// puis remplie le tableau let
-// console log
-// renvoie l'objet photographers
+// puis remplie deux tableaux let photographers et let media
+// renvoie les objets photographers et media
 async function getPhotographers() {
   await fetch("/data/photographers.json")
     .then((response) => response.json())
     .then((response2) => {
-      photographers = response2.photographers; //>>> si la let photographers != de constance)
-    });
-  return { photographers };
-}
-
-async function getMedia() {
-  await fetch("/data/photographers.json")
-    .then((response) => response.json())
-    .then((response2) => {
+      photographers = response2.photographers;
       media = response2.media; //>>> si la let photographers != de constance)
     });
-  return { media };
+  return { media }, { photographers };
 }
-// todo: comment savoir si ça marche? = je joue la fonction:
-// mais le console.log dans la fonction ne s'affiche pas. par
-// contre quand j'interroge la console, elle connait media et
-// c'est bien en tableau plein
-getMedia();
-
-// avec l'id, récupérer les autres données dans le tableau:
-// j'arrive à récupérer la partie de l'array dans la console avec la deuxième partie
-// de cet ordre.
-// TODO: en gros faut que je dise d'attendre d'avoir le tableau photographers : comment?
-// il faut mettre dans une fonction qui a fait appel au fonction précédente
-photographerData = photographers.filter((el) => el.id == parameterID);
-
-//Plan:
-//  OK=> récupérer l'adresse URl courante: https://www.journaldunet.fr/web-tech/developpement/1202481-comment-recuperer-le-chemin-de-l-url-courante-dans-jquery-et-l-assigner-a-une-variable/#:~:text=Pour%20cela%2C%20JavaScript%20poss%C3%A8de%20nativement,valeur%20de%20l'attribut%20pathname.
-//  OK =>récupérer l'id du paramètre pour afficher ses données dans le display : https://waytolearnx.com/2019/10/comment-recuperer-les-parametres-durl-en-javascript.html
-//  avec l'id, récupérer les autres données dans le tableau avec .find, .filter,
-//.indexOf, .includes: https://www.digitalocean.com/community/tutorials/js-array-search-methods-fr
-
-// Plan suite:
-// on a récupéré les infos
-// on affiche les infos
 
 async function displayData(photographer) {
   console.log(photographer);
-  const photographersSection =
-    document.getElementsByClassName("photograph-header");
-  // photographers.forEach((photographer) => {
-  //   const photographerModel = photographerFactory(photographer);
-  //   const userCardDOM = photographerModel.getUserCardDOM();
-  //   photographersSection.appendChild(userCardDOM);
-  // });
+  // const photographHeaderDiv =
+  //   document.getElementsByClassName("photograph-header");
+  // thomas: ça ne marche pas avec getElement mais ça marche avec query? Pourquoi?
+  const photographHeaderDiv = document.querySelector(".photograph-header");
+  photographer.forEach((id) => {
+    const photographerModel = photographerFactory(id);
+    const printPhotographHeader = photographerModel.getPhotographHeader();
+    photographHeaderDiv.appendChild(printPhotographHeader);
+  });
 }
 
 async function init() {
@@ -72,3 +43,7 @@ async function init() {
   displayData(photographerData);
 }
 init();
+
+// Plan suite:
+// on a récupéré les infos
+// on affiche les infos
