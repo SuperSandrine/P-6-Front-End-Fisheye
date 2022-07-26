e0b3583426603c5197c5f98ebed8646178246cda
SuperSandrine <sandrine.mestas@gmail.com>
2022-07-27 00:15:51 +0200

    Mise en place d'une select box avec js et un modèle W3S, facile à customisé, plus difficile à paramétrer, todo: rajouter des commentaires, changer les noms des let pour faciliter la lecture, ou trouver une autre solution.

--- scripts/utils/lightbox.js
+++ scripts/utils/lightbox.js
@@ -1,20 +1,18 @@
-// pour chaque image de la gallerie
-// quand je clique dessus
-// s'affiche en grand seule dans la lightbox
+// plan: quand je clique sur une une image de la gallerie
+// cette image s'affiche en grand seule dans la lightbox
 // dans la ligthbox je passe d'une image à l'autre avec les fleches.
-// soit je navique dans un array avec les flèches.
+// soit je navigue dans un array avec les flèches.
 
 const lightboxModal = document.getElementById("lightbox_modal");
 const lightboxMedia = document.querySelector(".lightbox_modal-content");
 
-let idMedia;
-let idArray;
+let idMedia; // pour stocker l'id du média onclick sur la gallerie
+let idArray; // pour stocker un tableau de tous les id des media d'un photographe
 
+// les 3 lets suivants pour remplir dynamiquement le innerhtml
 let fillMediaImageSource;
 let fillMediaVideoSource;
 let fillMediaTitle;
-
-let nextMediaIndex;
 
 // en paramètre du display "e", j'ai appelé l'id du média dans media.js
 // quand je clique sur la photo, je récupère l'index de l'image pour afficher
@@ -23,7 +21,6 @@ function displayLightboxModal(e) {
   lightboxModal.style.display = "block";
   idMedia = e;
   getIndexofMediasForLightbox(photographerMedia);
-  console.log("voici l'index du media affiché : " + indexOfMedia);
   giveLightboxItsMedias(photographerMedia);
   return idMedia;
 }
@@ -50,28 +47,14 @@ function getIndexofMediasForLightbox(array) {
 
 function nextMedia(array) {
   indexOfMedia = indexOfMedia + 1;
-  console.log("media suivant est : " + indexOfMedia);
   let quantityOfIndex = array.length - 1;
   if (indexOfMedia - 1 == quantityOfIndex) {
     indexOfMedia = 0;
-    console.log("on recommence");
-    //   } else if (fillMediaSuivantImageSource == undefined) {
-    //     let fillMediaImageSource;
-    //     console.log("c'est une vidéo");
-    // } else if (mediaSuivant > quantityOfIndex) {
-    //   console.log("fin du train");
-    // renvoie une erreur car ne peut pas lire l'array comme il n'existe pas
-    // intervenir avant la lecture
   } else {
-    console.log("c'est dns la pipe");
   }
-  // si mediaSuivant = undefined, alors on est au bout de l'array ou on est face à une vidéo
-  //et il faut sortir OU revenir au début
-  // (photographerMedia.length)-1 on obtient le nombre d'itérance
   fillMediaImageSource = array[indexOfMedia].image;
   fillMediaVideoSource = array[indexOfMedia].video;
   fillMediaTitle = array[indexOfMedia].title;
-  console.log(fillMediaTitle);
   return {
     indexOfMedia,
     fillMediaImageSource,
@@ -82,28 +65,14 @@ function nextMedia(array) {
 
 function previousMedia(array) {
   indexOfMedia = indexOfMedia - 1;
-  console.log("media précédent est : " + indexOfMedia);
   let quantityOfIndex = array.length - 1;
-  if (indexOfMedia == 0) {
+  if (indexOfMedia + 1 == 0) {
     indexOfMedia = quantityOfIndex;
-    console.log("on recommence");
-    //   } else if (fillMediaSuivantImageSource == undefined) {
-    //     let fillMediaImageSource;
-    //     console.log("c'est une vidéo");
-    // } else if (mediaSuivant > quantityOfIndex) {
-    //   console.log("fin du train");
-    // renvoie une erreur car ne peut pas lire l'array comme il n'existe pas
-    // intervenir avant la lecture
   } else {
-    console.log("c'est dns la pipe");
   }
-  // si mediaSuivant = undefined, alors on est au bout de l'array ou on est face à une vidéo
-  //et il faut sortir OU revenir au début
-  // (photographerMedia.length)-1 on obtient le nombre d'itérance
   fillMediaImageSource = array[indexOfMedia].image;
   fillMediaVideoSource = array[indexOfMedia].video;
   fillMediaTitle = array[indexOfMedia].title;
-  console.log(fillMediaTitle);
   return {
     indexOfMedia,
     fillMediaImageSource,
@@ -129,7 +98,7 @@ function giveLightboxItsMedias() {
 // dans la ligthbox je passe d'une image à l'autre avec les fleches.
 // soit je navigue dans un array avec les flèches.
 
-// je crée un listener sur le click du next button, qui joue la fonction next
+// je crée un listener sur le click du next button, qui joue la fonction next et rempli la lightbox
 const lightboxModalNext = document.querySelector("#lightbox_modal-next-button");
 lightboxModalNext.addEventListener("click", (e) => {
   nextMedia(photographerMedia);
@@ -143,54 +112,3 @@ lightboxModalPrevious.addEventListener("click", (e) => {
   previousMedia(photographerMedia);
   giveLightboxItsMedias();
 });
-
-// cette fonction affiche l'index du media actuel, l'index suivant et
-// le nom de la prochaine photo.
-// function nextMedia(array) {
-//   nextMediaIndex = indexOfMedia + 1;
-//   console.log("media suivant est : " + nextMediaIndex);
-//   let quantityOfIndex = array.length - 1;
-//   if (indexOfMedia == quantityOfIndex) {
-//     nextMediaIndex = 0;
-//     console.log("on recommence");
-//     //   } else if (fillMediaSuivantImageSource == undefined) {
-//     //     let fillMediaImageSource;
-//     //     console.log("c'est une vidéo");
-//     // } else if (mediaSuivant > quantityOfIndex) {
-//     //   console.log("fin du train");
-//     // renvoie une erreur car ne peut pas lire l'array comme il n'existe pas
-//     // intervenir avant la lecture
-//   } else {
-//     console.log("c'est dns la pipe");
-//   }
-//   // si mediaSuivant = undefined, alors on est au bout de l'array ou on est face à une vidéo
-//   //et il faut sortir OU revenir au début
-//   // (photographerMedia.length)-1 on obtient le nombre d'itérance
-//   fillMediaImageSource = array[nextMediaIndex].image;
-//   fillMediaVideoSource = array[nextMediaIndex].video;
-//   fillMediaTitle = array[nextMediaIndex].title;
-//   console.log(fillMediaTitle);
-//   return {
-//     nextMediaIndex,
-//     fillMediaImageSource,
-//     fillMediaVideoSource,
-//     fillMediaTitle,
-//   };
-// }
-
-// function getIndexOfNextMediasForLightbox(array) {
-//     fillMediaImageSource = array[nextMediaIndex].image;
-//     fillMediaVideoSource = array[nextMediaIndex].video;
-//     fillMediaTitle = array[nextMediaIndex].title;
-//     return {
-//       indexOfMedia,
-//       fillMediaImageSource,
-//       fillMediaVideoSource,
-//       fillMediaTitle,
-//     };
-
-// afficher le media suivant
-// j'ai une fonction qui créer l'index du media suivant,
-//maintenant au click, je l'affiche
-
-//function displayNextMedia();
